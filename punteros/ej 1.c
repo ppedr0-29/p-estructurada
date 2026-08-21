8b4a56995e089e502096180a0ada68046cf5046e
pedroldev <ploraschi29@gmail.com>
2026-08-21 20:25:59 -0300

    update

--- punteros/ej 1.c
+++ punteros/ej 1.c
@@ -7,7 +7,7 @@ c) el contenido de la variable
 d) el contenido de la variable accediendo mediante el puntero
 e) el contenido del puntero */
 
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 
 int main (){
@@ -15,12 +15,13 @@ int main (){
     int entero;
     int *pentero=&entero;
     *pentero=10;
-    
-    printf("La direccion de la variable es: %p\n", &entero);
     printf("La direccion del puntero es : %p\n", &pentero);
     printf("El contenido de la variable es : %d\n", entero);
     printf("El contenido de la variable mediante el puntero es: %d\n", *pentero);
     printf("El contenido del puntero es : %p\n", pentero);
 
+    printf("Hola");
+
     return 0;
+    
 }
