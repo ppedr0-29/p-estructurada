fb72df442f9a1e449cde10195633dbc913cddd4b
pedroldev <ploraschi29@gmail.com>
2026-08-26 19:36:19 -0300

    update ej5

--- punteros/ej 5.c
+++ punteros/ej 5.c
@@ -7,13 +7,31 @@ en qué posición del vector estaba (calcular dicha posición utilizando la dire
 
 void cargarVector(int *, int);
 void mostrarVector(int *, int);
+int *busqueda(int *, int , int );
 
 int main (){
     int v[10];
     int *pv=v;
-
+    int numBusqueda, posencontrada;
+    int *pos;
     cargarVector(pv, 10);
     mostrarVector(pv, 10);
+    printf("Ingrese el numero a buscar(>=0 corta): ");
+    scanf("%d", &numBusqueda);
+    while (numBusqueda>0)
+    {
+        pos=busqueda(pv, numBusqueda, 10);
+        if (pos==NULL)
+        {
+            printf("No se encontro. Reingrese:");
+        }else{
+            posencontrada=pos-pv;
+            printf("Se encontraba en la posicion %d", posencontrada);
+            printf("Ingrese el numero a buscar(>=0 corta): ");
+        }
+        scanf("%d", &numBusqueda);
+    }
+    printf("El programa finalizo.");
 
     return 0;
 }
@@ -32,4 +50,19 @@ void mostrarVector(int *pv, int ce){
         printf("El valor es %d en la posicion %d\n", *(pv+i), i+1);
     }
     
+}
+
+int *busqueda(int *pv, int num, int ce){
+    int i=0, pos=-1, *pr=NULL;
+    while (pos==-1 && i<ce)
+    {
+        if (*(pv+i)==num)
+        {
+            pos=i;
+            pr=pv+pos;
+        }else{
+            i++;
+        }
+    }
+    return pr;
 }
