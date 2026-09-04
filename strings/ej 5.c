0e8cfd3273ec710a2bea4cab0e82dd2283e10a8f
pedroldev <ploraschi29@gmail.com>
2026-09-04 11:53:50 -0300

    update

--- strings/ej 5.c
+++ strings/ej 5.c
@@ -15,14 +15,26 @@ b. Realizar el informe con el formato siguiente: */
 #define TAM 30
 
 int main(){
-    int dia, mes, anio;
-    int dias;
-    float precio, dolar;
-    int autos[TAM][7];
+    int dia, mes, anio, cantAlq;
+    int dias [TAM];
+    float precio [TAM], dolar, porcentaje;
+    char autos[TAM][7] = {
+    "AA111A","AA222B","AA333C","AA444D","AA555E",
+    "AA666F","AA777G","AA888H","AA999I","AB111J",
+    "AB222K","AB333L","AB444M","AB555N","AB666O",
+    "AB777P","AB888Q","AB999R","AC111S","AC222T",
+    "AC333U","AC444V","AC555W","AC666X","AC777Y",
+    "AC888Z","AC999A","AD111B","AD222C","AD333D"
+    };
+    cantAlq=cargaAlquiler(autos, dias, precio, TAM);
+    porcentaje= (float)(cantAlq/TAM)*100;
+    printf("El porcentaje de autos alquilados es %.2f%", porcentaje);
+
 
 
     return 0;
 }
+
 
 void procesamientoAlquileres(int *dia, int *mes, int *anio, float *dolar){
     printf("--PROCESAMIENTO DE ALQUILERES--\n");
@@ -41,26 +53,25 @@ int cargaAlquiler(char patente[][7], int *dias, float *precio, int ce){
     int pos;
     int alquilado[TAM]={0};
     char patenteAux[TAM];
-    printf("--ALQUILERES DEL DIA--");
+    printf("--ALQUILERES DEL DIA--\n");
     printf("Ingrese patente del auto: ");
     leeryValidarTexto(patenteAux, 7);
-    while (strcmpi(patenteAux, "FINDIA")!=0 && i<ce)
-    {   
+    while (strcmpi(patenteAux, "FINDIA")!=0 && i<ce){   
         pos=busqueda(patente, patenteAux, ce);
-        while (pos!=-1 && strcmpi(patenteAux, "FINDIA")!=0)
+        if(pos!=-1)
         {
             alquilado[pos]=1;
             printf("Ingrese cantidad de dias de alquiler: ");
-            leeyvalidaInt(&dias, 1);
+            leeyvalidaInt(&dias[pos], 1);
             printf("Ingrese precio en dolares: ");
-            leeyvalidaFloat(&precio, 1);
+            leeyvalidaFloat(&precio[pos], 1);
             i++;
+            printf("Ingrese patente del auto: ");
+        }else{
+            printf("Patente no encontrada. Reingrese: ");
         }
-        printf("Patente no encontrada.");
-        printf("Ingrese patente del auto: ");
         leeryValidarTexto(patenteAux, 7);
     }
-    
     return i;
 }
 
