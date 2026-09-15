d7380112407fd9fbd9a1d100c7a8176012af2e0b
pedroldev <ploraschi29@gmail.com>
2026-09-15 18:46:32 -0300

    update

---
+++ modelos 1Parcial/modelo 1-p2.c
@@ -0,0 +1,198 @@
+/*Una editorial desea gestionar su sistema de stock de libros. Para ello necesita manejar la siguiente información:
+Por cada libro:
+Código ISBN (entero de 8 dígitos)
+Título (50 caracteres máximo)
+Autor (40 caracteres máximo)
+Cantidad de unidades en stock (entero)
+La carga inicial de productos finaliza cuando se ingresa ISBN = 0.
+
+Gestión de memoria:
+a) Debido a que no se conoce la cantidad total de libros, se debe asignar un arreglo dinámico en memoria.
+b) El arreglo debe comenzar con una capacidad inicial de 10 elementos.
+c) Cada vez que el arreglo se llene, su capacidad debe aumentarse dinámicamente de 10 en 10.
+
+Al finalizar el mes, se ingresan las ventas con los siguientes datos:
+Código ISBN del libro
+Cantidad vendida
+Se pide hacer un programa que permita ingresar los datos de los libros considerando que se finaliza la carga cuando se ingresa el ISBN 0.
+Luego, permitir ingresar las ventas considerando que no se van a ingresar más ventas cuando se ingrese el ISBN 0.
+Se debe:
+a. Actualizar el stock de cada libro con las ventas realizadas en el mes.
+b. Al finalizar, mostrar el listado actualizado, informando:
+ISBN              TITULO               AUTOR                STOCK ACTUALIZADO
+99999999 XXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXX        9999*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int codigo;
+    char titulo[51];
+    char autor[41];
+    int cantStock;
+}LIBRO;
+
+typedef struct
+{
+    int cod;
+    int cantV;
+}VENTAS;
+
+LIBRO* INGRESO(LIBRO *datos, int *mem, int *cantL);
+LIBRO ingreso(LIBRO *datos);
+void ventas(VENTAS info, LIBRO *datos, int *ce);
+int busqueda(LIBRO *datos, int cod, int *ce);
+void listado(LIBRO *datos, int *ce);
+void leeyvalidaInt(int *dato, int lim);
+void leeyvalidaIntE2CF(int *dato, int min, int max, int cf);
+void leerTexto(char texto[], int largo);
+void leeryValidarTexto(char texto[], int largo);
+
+int main(){
+    LIBRO *datos;
+    VENTAS info;
+    int capInicial=10;
+    int cantLibros;
+    datos=(LIBRO*)malloc(capInicial*sizeof(LIBRO));
+    if (datos==NULL)
+    {
+        printf("Error al reservar memoria.");
+        exit(1);
+    }
+    datos= INGRESO(datos, &capInicial, &cantLibros);
+    ventas(info, datos, &capInicial);
+    listado(datos, &capInicial);
+
+    free(datos);
+
+    return 0;
+}
+
+void listado(LIBRO *datos, int *ce){
+    printf("%8s %-20s %-20s %17s\n", "ISBN", "TITULO", "AUTOR", "STOCK ACTUALIZADO");
+    for (int i = 0; i < *ce; i++)
+    {
+        printf("%8d %-20s %-20s %17d\n", (datos+i)->codigo, (datos+i)->titulo, (datos+i)->autor ,(datos+i)->cantStock);
+    }
+}
+
+void ventas(VENTAS info, LIBRO *datos,int *ce){
+    VENTAS aux;
+    int pos;
+    printf("--ACTUALIZACION STOCK--\n");
+    printf("Ingrese codigo ISBN: ");
+    leeyvalidaIntE2CF(&aux.cod, 10000000, 99999999, 0);
+    while (aux.cod!=0)
+    {
+        pos=busqueda(datos, aux.cod, ce);
+        if (pos!=-1)
+        {   
+            printf("Ingrese cantidad vendida: ");
+            leeyvalidaInt(&aux.cantV, 1);
+            (datos+pos)->cantStock-=aux.cantV;
+        }else{
+            printf("El codigo no existe. ");
+        }
+        printf("Ingrese codigo ISBN: ");
+        leeyvalidaIntE2CF(&aux.cod, 10000000, 99999999, 0);
+    }
+    printf("--Actualizacion finalizada.--");
+}
+
+int busqueda(LIBRO *datos, int cod, int *ce){
+    int pos=-1, i=0;
+    while (pos==-1 && i<*ce)
+    {
+        if ((datos+i)->codigo==cod)
+        {
+            pos=i;
+        }
+        i++;
+    }
+    return pos;
+}
+
+LIBRO* INGRESO(LIBRO *datos, int *mem, int *cantL){
+    LIBRO aux;
+    int i=0;
+    printf("--INGRESO LIBROS--\n");
+    aux=ingreso(datos);
+    while (aux.codigo!=0)
+    {
+        if(i==*mem){
+            *mem+=10;
+            datos=(LIBRO*)realloc(datos, *mem*sizeof(LIBRO));
+            if (datos==NULL)
+            {
+                printf("Error al reservar memoria.");
+                exit(1);
+            }
+        }
+        *(datos+i)=aux;
+        i++;
+        aux=ingreso(datos);
+    }
+    *cantL=i;
+    return datos;
+}
+
+LIBRO ingreso(LIBRO *datos){
+    LIBRO aux;
+    printf("Ingrese codigo ISBN: ");
+    leeyvalidaIntE2CF(&aux.codigo, 10000000, 99999999, 0);
+    if (aux.codigo!=0)
+    {
+        printf("Ingrese titulo del libro: ");
+        leeryValidarTexto(aux.titulo, 51);
+        printf("Ingrese autor del libro: ");
+        leeryValidarTexto(aux.autor, 41);
+        printf("Ingrese cantidad de stock: ");
+        leeyvalidaInt(&aux.cantStock, 0);
+    }
+    return aux;
+}
+
+void leeyvalidaInt(int *dato, int lim){
+    scanf("%d", dato);
+    while (*dato<lim)
+    {
+        printf("Error. Reingrese: ");
+        scanf("%d", dato);
+    }
+}
+
+void leeyvalidaIntE2CF(int *dato, int min, int max, int cf){
+    scanf("%d", dato);
+    while ((*dato<min || *dato>max) && *dato!=cf)
+    {
+        printf("Error. Reingrese: ");
+        scanf("%d", dato);
+    }
+}
+
+void leerTexto(char texto[], int largo){
+    int i=0;
+    fflush(stdin);
+    fgets(texto, largo, stdin);
+    while (texto[i]!='\0')
+    {
+        if (texto[i]=='\n')
+        {
+            texto[i]='\0';
+        }
+        i++;
+    }
+
+}
+
+void leeryValidarTexto(char texto[], int largo){
+    leerTexto(texto, largo);
+    while (strlen(texto)==0)
+    {
+        printf("Error. Reingrese: ");
+        leerTexto(texto, largo);
+    }
+
+}
